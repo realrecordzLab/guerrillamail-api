625151d4e7a488c78fbfcb276e8cd5c1f04c786d
realrecordzLab <51746386+realrecordzLab@users.noreply.github.com>
2021-03-05 11:10:48 +0100

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,17 +1,17 @@
-export function partiallyApply(fn, ...initialArgs) {
+function partiallyApply(fn, ...initialArgs) {
 	return function partiallyApplied(...laterArgs) {
 		return fn(...initialArgs, ...laterArgs);
 	};
 }
 
-export function getMostRecentEmail(emailList) {
+function getMostRecentEmail(emailList) {
 	if (Array.isArray(emailList) && emailList.length) {
 		return emailList.pop();
 	}
 	return false;
 }
 
-export function filterNewEmails(oldEmails, possibleNewEmails) {
+function filterNewEmails(oldEmails, possibleNewEmails) {
 	if (!oldEmails.length) {
 		// We don't have any old emails, so all of them must be new
 		return possibleNewEmails;
@@ -21,6 +21,11 @@ export function filterNewEmails(oldEmails, possibleNewEmails) {
 	return possibleNewEmails.filter(newEmail => !oldEmailIds.includes(newEmail.mail_id));
 }
 
-export function syncLocalEmails(localEmails, emailIds) {
+function syncLocalEmails(localEmails, emailIds) {
 	return localEmails.filter(email => !emailIds.includes(email.mail_id));
 }
+
+module.exports.partiallyApply = partiallyApply;
+module.exports.getMostRecentEmail = getMostRecentEmail;
+module.exports.filterNewEmails = filterNewEmails;
+module.exports.syncLocalEmails = syncLocalEmails;
